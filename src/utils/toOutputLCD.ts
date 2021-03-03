e71acc5e147cb2e444d5f0ed74db7aa639aec766
Karl Golka <contact@karlgolka.com>
2021-03-03 14:39:43 -0800

    feat: fs appending to LCD output on sale

--- src/utils/toOutputLCD.ts
+++ src/utils/toOutputLCD.ts
@@ -1,7 +1,12 @@
-const toOutputLCD = async (contents: string)=> {
+import fs from 'fs';
+
+const toOutputLCD = async (contents: string) => {
     // faux print to LCD output (a file)
     console.log("🖥  ~ contents", contents)
-    
+
+    const outputFilename = `output-LCD.txt`; // NOTE: workaround for paths needed when running on my env
+    const fs = require('fs');
+    fs.appendFileSync(outputFilename, contents + '\n');
     return;
 
 };
